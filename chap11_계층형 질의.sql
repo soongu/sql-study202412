c7acda36fd43e4c785fe3764479527764b999a53
Soon9 (SoonGu Hong) <naong526@naver.com>
2025-01-06 17:16:48 +0900

    chap11 계층형 질의

---
+++ chap11_계층형 질의.sql
@@ -0,0 +1,40 @@
+
+-- 계층형 쿼리 
+-- START WITH : 계층의 첫 단계를 어디서 시작할 것인지의 대한 조건
+-- CONNECT BY PRIOR 자식 = 부모  -> 순방향 탐색
+-- CONNECT BY 자식 = PRIOR 부모  -> 역방향 탐색
+-- ORDER SIBLINGS BY : 같은 레벨끼리의 정렬을 정함.
+SELECT 
+    LEVEL AS LVL,
+    LPAD(' ', 4*(LEVEL-1)) || emp_no || '(' || emp_nm || ')' AS "조직인원",
+    A.dept_cd,
+    B.dept_nm,
+    A.emp_no,
+    A.direct_manager_emp_no,
+    CONNECT_BY_ISLEAF
+FROM tb_emp A
+JOIN tb_dept B
+ON A.dept_cd = B.dept_cd
+START WITH A.direct_manager_emp_no IS NULL
+-- START WITH A.EMP_NO = '1000000037'
+CONNECT BY PRIOR A.emp_no = A.direct_manager_emp_no
+ORDER SIBLINGS BY A.emp_no DESC
+;
+
+
+
+
+SELECT 
+    LEVEL AS LVL,
+    LPAD(' ', 4*(LEVEL-1)) || emp_no || '(' || emp_nm || ')' AS "조직인원",
+    A.dept_cd,
+    B.dept_nm,
+    A.emp_no,
+    a.direct_manager_emp_no
+FROM tb_emp A
+JOIN tb_dept B
+ON A.dept_cd = B.dept_cd
+START WITH A.emp_no = '1000000037'
+CONNECT BY A.emp_no = PRIOR A.direct_manager_emp_no;
+
+
